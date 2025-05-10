6f2a94fff18ffe5ef8835a8017304e433913f5e1
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2025-05-10 10:01:11 +0700

    Update app.blade.php

--- halo_pet_api/resources/views/layouts/app.blade.php
+++ halo_pet_api/resources/views/layouts/app.blade.php
@@ -18,26 +18,68 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="/">Halo Pet</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item {{ request()->is('home') ? 'active' : '' }}">
-                            <a class="nav-link" href="/home">Home</a>
-                        </li>
-                        <li class="nav-item {{ request()->is('doctors*') ? 'active' : '' }}">
-                            <a class="nav-link" href="/doctors">Doctors</a>
-                        </li>
-                        <li class="nav-item {{ request()->is('hospitals*') ? 'active' : '' }}">
-                            <a class="nav-link" href="/hospitals">Hospitals</a>
-                        </li>
-                        <li class="nav-item {{ request()->is('shops*') ? 'active' : '' }}">
-                            <a class="nav-link" href="/shops">Shops</a>
-                        </li>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                            </li>
+                            <li class="nav-item {{ request()->is('doctors*') ? 'active' : '' }}">
+                                <a class="nav-link" href="/doctors">Doctors</a>
+                            </li>
+                            <li class="nav-item {{ request()->is('hospitals*') ? 'active' : '' }}">
+                                <a class="nav-link" href="/hospitals">Hospitals</a>
+                            </li>
+                            <li class="nav-item {{ request()->is('shops*') ? 'active' : '' }}">
+                                <a class="nav-link" href="/shops">Shops</a>
+                            </li>
+                        @endauth
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
